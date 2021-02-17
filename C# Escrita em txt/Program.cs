4173c765d0b96f269b7edf31b00237ee6e659043
Renatosnp1 <renato_snp@yahoo.com.br>
2021-02-16 23:26:48 -0300

    Primeiro coodigo escrita txt

---
+++ C# Escrita em txt/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace aula_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using(System.IO.TextWriter w = System.IO.File.CreateText("texto.txt"))
+            {
+                for (int i = 0; i <= 1000; i++)
+                {
+                    string num1 = Convert.ToString(i);
+                    w.WriteLine("Linha" + num1);
+                }
+                w.Dispose();
+            }
+
+        }
+    }
+}
