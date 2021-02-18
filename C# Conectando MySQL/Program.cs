da229d32f260cccd99a530742d69f2d0f79ebf5b
Renatosnp1 <renato_snp@yahoo.com.br>
2021-02-17 21:42:48 -0300

    Adicionando pastas input e mysql

---
+++ C# Conectando MySQL/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+
+namespace C__Conectando_MySQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+        }
+    }
+}
---
+++ C# Input dados/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace C__Input_dados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Qual e seu nome?");
+
+            String nome = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine("Seja bem vindo {0}",nome);
+
+
+        }
+    }
+}
