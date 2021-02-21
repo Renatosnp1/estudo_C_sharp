c2adc584b5e6c60c71463260358a6f8eca214164
Renatosnp1 <renato_snp@yahoo.com.br>
2021-02-20 21:29:41 -0300

    Inclusao pasta nomes parecidos

---
+++ C# Nomes parecidos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace C__Nomes_parecidos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] names_vector = {"renato", "renata", "clara", "vitoria", "luciola"};
+
+            foreach(string nome in names_vector)
+            {
+                if(nome.Contains('a'))
+                {
+                    
+                }
+            }
+
+        }
+    }
+}
---
+++ C# Vetores-Matrizes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace C__Vetores_Matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Qual e seu Nome?");
+            // string nome = Console.ReadLine();
+            string nome = "Renato Vinicius Carneiro De Mattos";
+
+
+            string[] nome_split = nome.Split(' ');
+
+
+            for (int i = 0; i < nome.Length ; i++)
+            {
+                Console.WriteLine(nome[i]);
+            }
+            
+            foreach(String n in nome_split){
+                Console.WriteLine(n);
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
